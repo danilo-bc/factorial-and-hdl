797c5051f016e1925b9e295ef71dc7639cd23a7e
Danilo <danilo-bc@users.noreply.github.com>
2023-05-22 22:17:06 -0300

    Added SystemVerilog block and testbench

---
+++ src/FactorialBlk.sv
@@ -0,0 +1,76 @@
+module FactorialBlk(
+    input logic clk,
+    input logic resetn,
+    input logic [3:0] in_data,
+    input logic in_valid,
+    output logic [45:0] out_data,
+    output logic out_valid,
+    output logic out_busy
+    );
+
+    localparam INIT = 2'b00;
+    localparam CALC = 2'b01;
+    localparam DONE = 2'b10;
+
+    logic [1:0] curr_state;
+    logic [1:0] next_state;
+
+    logic input_valid;
+    logic [45:0] calc_buffer;
+    logic [3:0] counter;
+
+    always_comb begin
+        case (curr_state)
+            INIT: begin
+                if (in_valid) next_state = CALC;
+                else next_state = INIT;
+            end
+            CALC: begin
+                if (counter == 1) next_state = DONE;
+                else next_state = CALC;
+            end
+            DONE: begin
+                next_state = INIT;
+            end
+        endcase
+    end
+
+    always_ff @(posedge clk or negedge resetn) begin
+        if(!resetn) begin
+            curr_state <= INIT;
+        end
+        else begin
+            curr_state <= next_state;
+        end
+    end
+
+    always_ff @(posedge clk or negedge resetn) begin
+        if (!resetn) begin
+            calc_buffer <= 46'b1;
+            counter <= 4'b0;
+            out_valid <= 1'b0;
+            out_busy <= 1'b0;
+        end
+        else begin
+            case (curr_state)
+                INIT: begin
+                    if (in_valid) begin
+                        counter <= in_data;
+                        out_valid <= 1'b0;
+                        out_busy <= 1'b1;
+                    end
+                end
+                CALC: begin
+                    calc_buffer <= calc_buffer * counter;
+                    counter <= counter - 1;
+                end
+                DONE: begin
+                    out_data <= calc_buffer;
+                    calc_buffer <= 46'b1;
+                    out_valid <= 1'b1;
+                    out_busy <= 1'b0;
+                end
+            endcase
+        end
+    end
+endmodule
---
+++ src/FactorialBlk_TB.sv
@@ -0,0 +1,50 @@
+module FactorialBlk_TB();
+
+logic clk;
+logic resetn;
+logic [3:0] in_data;
+logic in_valid;
+logic [45:0] out_data;
+logic out_valid;
+logic out_busy;
+
+FactorialBlk dut (
+    .clk(clk),
+    .resetn(resetn),
+    .in_data(in_data),
+    .in_valid(in_valid),
+    .out_data(out_data),
+    .out_valid(out_valid),
+    .out_busy(out_busy)
+);
+
+always begin
+    #5 clk = ~clk;
+end
+
+initial begin
+    clk = 0;
+    resetn = 0;
+    in_data = 0;
+    in_valid = 0;
+
+    // Reset
+    #5 resetn = 1;
+
+    // Calculate factorial for input 3
+    #5 in_data = 3;
+    in_valid = 1;
+
+    @(posedge out_valid);
+    $display("Factorial of 3: %0d", out_data);
+    // Calculate factorial for input 5
+    in_data = 5;
+    #5
+
+    @(posedge out_valid);    
+    $display("Factorial of 5: %0d", out_data);
+
+    #5 $finish;
+end
+
+endmodule
